3cfd3d215a70fddcdbdbd426521f3e5ace091d0f
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2020-04-02 10:37:23 +0700

    sidekiq

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'sidekiq'
+require 'sidekiq/web'
+
+redis_params = {
+  # url: ENV['REDIS_URL_SIDEKIQ']
+  { url: 'redis://localhost:6379/0' }
+}
+
+# Stats:
+# concurrency: sidekiq client does not need concurrency options
+# size: 5 - default
+Sidekiq.configure_client do |config|
+  config.redis = redis_params
+end
+
+# Stats:
+# concurrency: 5
+# size: 10 ( concurrency + 5 )
+Sidekiq.configure_server do |config|
+  config.redis = redis_params
+end
